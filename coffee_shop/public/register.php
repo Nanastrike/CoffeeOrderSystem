6d0371a889b8352433480babc70bf4338d0931d9
Anyi1818 <anyiding28@gmail.com>
2024-11-22 13:14:33 -0500

    Update register.php

--- coffee_shop/public/register.php
+++ coffee_shop/public/register.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>register</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Login or Register</title>
+    <link rel="stylesheet" href="style.css">
     <script src="script.js" defer></script>
 </head>
-<body>
 
 
 </form>
@@ -15,18 +14,21 @@
 
 <!-- TO DO: add a user name input
 the password need a double check, so the js validation will work-->
-    <form id="register-form" method="POST" action="../private/signUp.php" name="register-form">
-        <h2>Register</h2>
-        <label for="register-name">name:</label>
-        <input type="text" id="register-name" name="name" class="input-field" placeholder="Enter your email" >
-        <label for="register-email">Email:</label>
-        <input type="email" id="register-email" name="email" class="input-field" placeholder="Enter your email" >
-        <label for="register-password">Password:</label>
-        <input type="password" id="register-password" name="password" class="input-field" placeholder="Create a password" >
-        <button type="submit" class="btn" name="register-submit">Register</button>
-    </form>
-
-
+    <form id="register-form" method="post" action="register.php" name="register-form">
+                <div class="register-header">
+                    <h2 class="register-title">Register</h2>
+                    <a href="index.html" class="login-btn">Login ></a>
+                </div>
+                <label for="register-name">name:</label>
+                <input type="text" id="register-name" name="name" class="input-field" placeholder="Enter your email" >
+                <label for="register-email">Email:</label>
+                <input type="email" id="register-email" name="email" class="input-field" placeholder="Enter your email" >
+                <label for="register-password">Password:</label>
+                <input type="password" id="register-password" name="password" class="input-field" placeholder="input your password" >
+                <label for="register-password-again">Password:</label>
+                <input type="password" id="register-password-again" name="password-again" class="input-field" placeholder="input your password again" >
+                <button type="submit" class="btn" name="register-submit">Register</button>
+            </form>
     <!--TO DO: add a fixed footer php file
         <?php include 'footerEm.php'; ?> 
         -->
